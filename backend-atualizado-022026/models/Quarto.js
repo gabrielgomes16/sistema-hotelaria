6b3ffe4d43df4992fc0af4f62e65f42a2c13d2c5
gabrielgomes16 <gabrielsouzagomes2011@hotmail.com>
2026-03-15 17:25:01 -0300

    feat:esqueci de mandar atualizado[004]

--- backend-atualizado-022026/models/Quarto.js
+++ backend-atualizado-022026/models/Quarto.js
@@ -4,7 +4,14 @@ const sequelize = require('../config/database');
 const Quarto = sequelize.define('Quarto', {
   id_quarto: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   descricao: { type: DataTypes.STRING(255) },
-  status : { type: DataTypes.STRING(50) },
+  status: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    defaultValue: 'disponível',
+    validate: {
+      isIn: [['disponível', 'ocupado', 'manutenção']]
+    }
+  },
   numero: { type: DataTypes.STRING(10), allowNull: false },
   tipo: { type: DataTypes.STRING(50) },
   preco: { type: DataTypes.DECIMAL(10, 2) }
